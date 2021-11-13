a59300c778e7b5adcb8304792c210a2b5c0f2d98
matias <matias3282@hotmail.com>
2021-11-13 10:30:34 -0300

    Se inicia el proyecto

---
+++ contact/contacto.js
@@ -0,0 +1,34 @@
+function contactComponent(el) {
+  const componentEl = document.createElement("div");
+
+  componentEl.innerHTML = `
+  <section class="contacto">
+    <h2 class="contacto__title">Escribime</h2>
+    <form class="contacto__form">
+      <label>
+        <h3 class="contacto__label">Nombre</h3>   
+        <input class="contacto__input" type="text">
+      </label>
+      <label>
+       <h3 class="contacto__label">Email</h3>   
+       <input class="contacto__input" type="text">
+     </label>
+     <label>
+       <h3 class="contacto__label">Mensaje</h3>   
+       <textarea class="contacto__input-mensaje"></textarea>
+     </label>
+     <div class="contacto__submit-cont">
+       <button class="contacto__submit-button">Enviar</button>
+     </div>
+    </form>
+   </section>
+   `;
+
+  const form = componentEl.querySelector(".contacto__form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("el form se envio");
+  });
+
+  el.appendChild(componentEl);
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+function main() {
+  const container = document.querySelector(".contact__container");
+  contactComponent(container);
+}
+
+main();
