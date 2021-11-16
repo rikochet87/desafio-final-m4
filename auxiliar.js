77fa5b6533a84ed5eb7aa3874776220eeb0e779d
matias <matias3282@hotmail.com>
2021-11-16 18:46:21 -0300

    resolucion del header con css y js (ventana emergente)

---
+++ auxiliar.js
@@ -0,0 +1,13 @@
+function main() {
+  const buttonBurgerEL = document.querySelector(".header__burger-button");
+  const buttonBurgerCloseEl = document.querySelector(".window__close-button");
+  const windowEl = document.querySelector(".window");
+  buttonBurgerEL.addEventListener("click", () => {
+    windowEl.style.display = "inherit";
+  });
+  buttonBurgerCloseEl.addEventListener(
+    "click",
+    () => (windowEl.style.display = "none")
+  );
+}
+main();
