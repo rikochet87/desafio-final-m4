890a9787b8ea0ba45f022202863c5a36b2638fa3
matias <matias3282@hotmail.com>
2021-11-17 20:07:40 -0300

    comienzo a trabajar en el footer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function main() {
-  const containerHeader = document.querySelector(".header__cont");
+  const containerHeader = document.querySelector(".header__container");
   headerComponent(containerHeader);
   const container = document.querySelector(".contact__container");
   contactComponent(container);
+  const containerFooter = document.querySelector(".footer__container");
+  contactComponent(ontainerFooter);
 }
 
 main();
