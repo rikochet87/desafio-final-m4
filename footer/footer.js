88efc6b20fd868cd5454c6e91c5adadf51ae6642
matias <matias3282@hotmail.com>
2021-11-20 08:59:34 -0300

    se quito código que tenía de más, header, contact y footer terminados

--- footer/footer.js
+++ footer/footer.js
@@ -1,31 +1,39 @@
 function footerComponent(el) {
   const componentEl = document.createElement("div");
 
-  componentEl.innerHTML = ` <section class="footer">
-    <a href="./index.html" class="footer__link-logo">
+  componentEl.innerHTML = `<section class="footer">
+  <a href="./index.html" class="footer__link-logo">
+    <img
+      src="./imagenes/LogoMakr-0crHQu.png"
+      alt="logo"
+      class="footer__logo"
+    />
+  </a>
+  <div class="footer__social-media-section">
+    <div class="footer__social-media">
       <img
-        src="./imagenes/LogoMakr-0crHQu.png"
-        alt="logo"
-        class="footer__logo"
+        src="./imagenes/instagram.png"
+        alt=""
+        class="footer__instagram"
       />
-    </a>
-    <div class="footer__social-media-section">
-      <div class="footer__social-media">
-        <img src="./imagenes/instagram (2).png" alt="" class="footer__instagram">
-        <span class="footer__social-media-name">Instagram</span>
-      </div>
-
-      <div class="footer__social-media">
-        <img src="./imagenes/linkedin (3).png" alt="" class="footer__linkedin">
-        <span class="footer__social-media-name">Linkedin</span>
-      </div>
-
-      <div class="footer__social-media">
-          <img src="./imagenes/github (1).png" alt="" class="footer__github">
-          <span class="footer__social-media-name">GitHub</span>
-      </div>
+      <span class="footer__social-media-name">Instagram</span>
     </div>
-  </section>`;
+
+    <div class="footer__social-media">
+      <img
+        src="./imagenes/linkedin.png"
+        alt=""
+        class="footer__linkedin"
+      />
+      <span class="footer__social-media-name">Linkedin</span>
+    </div>
+
+    <div class="footer__social-media">
+      <img src="./imagenes/github.png" alt="" class="footer__github" />
+      <span class="footer__social-media-name">GitHub</span>
+    </div>
+  </div>
+</section> `;
 
   el.appendChild(componentEl);
 }
