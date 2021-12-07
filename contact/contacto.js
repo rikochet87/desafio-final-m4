c5b0ac9692aaf727aa49acc91d0c03d6f4144845
matias <matias3282@hotmail.com>
2021-12-07 16:23:00 -0300

    finalizando el trabajo

--- contact/contacto.js
+++ contact/contacto.js
@@ -6,15 +6,15 @@ function contactComponent(el) {
     <h2 class="contacto__title">Contacto</h2>
     <form class="contacto__form">
       <label>
-        <h3 class="contacto__label">Nombre</h3>   
+        <h3 class="contacto__label">Nombre</h3>
         <input class="contacto__input" type="text">
       </label>
       <label>
-       <h3 class="contacto__label">Email</h3>   
+       <h3 class="contacto__label">Email</h3>
        <input class="contacto__input" type="text">
      </label>
      <label>
-       <h3 class="contacto__label">Mensaje</h3>   
+       <h3 class="contacto__label">Mensaje</h3>
        <textarea class="contacto__input-mensaje"></textarea>
      </label>
      <div class="contacto__submit-cont">
@@ -24,11 +24,43 @@ function contactComponent(el) {
    </section>
    `;
 
-  const form = componentEl.querySelector(".contacto__form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log("el form se envio");
-  });
+  // const form = componentEl.querySelector(".contacto__form");
+  // form.addEventListener("submit", function (e) {
+  //   e.preventDefault();
+
+  //   console.log("el form se envio");
+  // });
 
   el.appendChild(componentEl);
+}
+
+function sendForm() {
+  const formEl = document.querySelector(".contacto__form");
+
+  formEl.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const formData = new FormData(evento.target);
+    const datosObj = Object.fromEntries(formData.entries());
+
+    fetch("https://apx-api.vercel.app/api/utils/dwf", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+
+      body: JSON.stringify({
+        to: "matias3282@hotmail.com",
+        message: `
+        Nombre: ${datosObj.nombre} <br> <br>
+        Email: ${datosObj.email} <br> <br>
+        Mensaje: ${datosObj.mensaje}
+        `,
+      }),
+    });
+    formEl.reset();
+    alert(
+      "Mensaje enviado correctamente, Gracias " +
+        datosObj.nombre +
+        " por comunicarte."
+    );
+  });
 }
