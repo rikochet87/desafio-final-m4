4c25871fa50cc9511fee30c7074f836215eebca1
matias <matias3282@hotmail.com>
2021-12-18 11:05:20 -0300

    todos los script con defer, correccion en el orden de los mismos y arreglo en header.js

--- header/header-window.js
+++ header/header-window.js
@@ -1,13 +1,12 @@
-function main() {
-  const buttonBurgerEL = document.querySelector(".header__burger-button");
-  const buttonBurgerCloseEl = document.querySelector(".window__close-button");
-  const windowEl = document.querySelector(".window");
-  buttonBurgerEL.addEventListener("click", () => {
-    windowEl.style.display = "inherit";
-  });
-  buttonBurgerCloseEl.addEventListener(
-    "click",
-    () => (windowEl.style.display = "none")
-  );
-}
-main();
+// function main() {
+//   const buttonBurgerEL = document.querySelector(".header__burger-button");
+//   const buttonBurgerCloseEl = document.querySelector(".window__close-button");
+//   const windowEl = document.querySelector(".window");
+//   buttonBurgerEL.addEventListener("click", () => {
+//     windowEl.style.display = "inherit";
+//   });
+//   buttonBurgerCloseEl.addEventListener("click", () => {
+//     windowEl.style.display = "none";
+//   });
+// }
+// main();
--- header/header.js
+++ header/header.js
@@ -49,4 +49,13 @@ function headerComponent(el) {
      `;
 
   el.appendChild(componentEl);
+  const buttonBurgerEL = document.querySelector(".header__burger-button");
+  const buttonBurgerCloseEl = document.querySelector(".window__close-button");
+  const windowEl = document.querySelector(".window");
+  buttonBurgerEL.addEventListener("click", () => {
+    windowEl.style.display = "inherit";
+  });
+  buttonBurgerCloseEl.addEventListener("click", () => {
+    windowEl.style.display = "none";
+  });
 }
